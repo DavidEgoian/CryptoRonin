92dff9c0868cfbf3bff95f7b8b36ab28880f0015
Dato Egoyan <93599837+datoegoyan@users.noreply.github.com>
2022-05-25 22:21:22 +0400

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let x = setInterval(function () {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Mint is Live now!";
+    document.getElementById("demo").innerHTML = "Mint was canceled :(";
   }
 }, 1000);
 
